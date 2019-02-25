ea6cafa06502b89b5f6a0f7265b8997f95ec48e4
fjt1509 <32302512+fjt1509@users.noreply.github.com>
2019-02-25 22:04:50 +0100

    fixed le problem

--- app/src/main/java/com/example/diceroll/MainActivity.java
+++ app/src/main/java/com/example/diceroll/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.diceroll;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.example.diceroll.Model.Dice;
@@ -20,10 +20,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     EditText numberDice;
-    TextView errTxt;
+    TextView txtOutput;
     ImageView testImageView;
 
     List<Dice> diceList;
+    int[] imageRefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +35,22 @@ public class MainActivity extends AppCompatActivity {
 
         diceList = dc.getDiceList();
 
-        Dice testDice = diceList.get(1);
-        testImageView = this.findViewById(R.id.testImageView);
-        testImageView.setImageResource(testDice.getImage());
-
-
-
-
         Button rollBtn = this.findViewById(R.id.btnRoll);
         numberDice = this.findViewById(R.id.numberDice);
-        errTxt = this.findViewById(R.id.txtError);
+        numberDice.setCursorVisible(false);
+        txtOutput = this.findViewById(R.id.txtOutput);
 
+        imageRefs = new int[] {R.id.imageView1, R.id.imageView2, R.id.imageView3, R.id.imageView4, R.id.imageView5, R.id.imageView6};
+        resetDice();
         rollBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                resetDice();
+
                 try
                 {
-                    int valueSum;
+                    int valueSum = 0;
                     int numberOfDice = Integer.parseInt(numberDice.getText().toString().trim());
 
                     if(numberOfDice >= 1 && numberOfDice <=6)
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                         {
                             int diceValue = generateDiceValue();
                             Dice correspondingDice = null;
-
+                            valueSum = valueSum + diceValue;
                             for (Dice dice : diceList)
                             {
                                 if(dice.getDiceValue() == diceValue)
@@ -69,9 +69,21 @@ public class MainActivity extends AppCompatActivity {
                                 }
                             }
 
-                            ImageView imageView = findViewById(getResources().getIdentifier("imageView" + i, "drawable", getPackageName()));
+                            ImageView imageView = (ImageView) findViewById(imageRefs[i-1]);
+                            imageView.setVisibility(View.VISIBLE);
                             imageView.setImageResource(correspondingDice.getImage());
+
                         }
+
+                        txtOutput.setText(valueSum + "");
+                        txtOutput.setTextColor(Color.parseColor("#000000"));
+
+
+                    }
+                    else
+                    {
+                        txtOutput.setText("Please Enter a number between 1 & 6");
+                        txtOutput.setTextColor(Color.parseColor("#DC143C"));
                     }
 
                 } catch (Exception ex)
@@ -87,6 +99,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+    }
+
+    private void resetDice() {
+
+        for (int i = 0; i < imageRefs.length; i++)
+        {
+            ImageView imageView = (ImageView) findViewById(imageRefs[i]);
+            imageView.setVisibility(View.INVISIBLE);
+        }
 
     }
 
